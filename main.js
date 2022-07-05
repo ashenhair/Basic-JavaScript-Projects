04ac407d87061fa7b08ee54427ea5731b23af193
Ashenhair <100282383+ashenhair@users.noreply.github.com>
2022-07-05 14:01:44 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+const starsEl = document.querySelectorAll(".fa-solid");
+const emojisEl = document.querySelectorAll(".fa-regular");
+
+starsEl.forEach((starsEl, index) => {
+    starsEl.addEventListener("click", () => {
+        updateRating(index);
+
+    });
+});
+
+function updateRating(index) {
+    starsEl.forEach((starEl, idx) => {
+        if (idx < index + 1) {
+            starEl.classList.add("active");
+        } else {
+            starEl.classList.remove("active");
+        }
+    });
+
+    emojisEl.forEach((emojiEl) => {
+        emojiEl.style.transform = 
+        `translateX(-${index * 50}px)`;
+    });
+}
+
